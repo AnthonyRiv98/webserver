d4e4b2b3880f7f253721995c071f23c92e8c80fb
AnthonyRiv98 <55204439+AnthonyRiv98@users.noreply.github.com>
2019-10-30 15:15:40 -0400

    newer

--- action.php
+++ action.php
@@ -2,7 +2,12 @@
 <html>
     <head>
         <div>
-            Hello <?php echo htmlspecialchars($_POST['fname']); ?>
- 
+            Hello <?php echo htmlspecialchars($_POST['fname']) htmlspecialchars($_POST['lname']); ?>
         </div>
+
+        <?php if ($_POST['eth'])
+            elseif ($_POST['usb3'])
+            elseif ($_POST['pwr'])
+            you have a raspberry pi 4 B+ model
+
     </head>
