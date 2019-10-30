8a25ef04e9381b28e9d4b1d02a9c364ad3a41293
AnthonyRiv98 <55204439+AnthonyRiv98@users.noreply.github.com>
2019-10-30 15:33:19 -0400

    Update action.php

--- action.php
+++ action.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <div>
-            Hello <?php echo (htmlspecialchars($_POST['fname']), htmlspecialchars($_POST['lname'])); ?>
+            Hello <?php echo htmlspecialchars($_POST['fname']), htmlspecialchars($_POST['lname']); ?>
         </div>
 
         <?php if ((int)($_POST['eth']) && (int)($_POST['usb3']) && (int)($_POST['pwr'])): ?>
