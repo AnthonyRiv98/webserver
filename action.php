4d5d314f6613eb1d7b158d798730f367c36c5a18
AnthonyRiv98 <55204439+AnthonyRiv98@users.noreply.github.com>
2019-10-30 15:36:47 -0400

    Update action.php

--- action.php
+++ action.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <div>
-            Hello <?php echo htmlspecialchars($_POST['fname']), htmlspecialchars($_POST['lname']); ?>
+            Hello <?php echo htmlspecialchars($_POST['fname']), " ", htmlspecialchars($_POST['lname']); ?>
         </div>
-
-        <?php if ((int)($_POST['eth']) && (int)($_POST['usb3']) && (int)($_POST['pwr'])): ?>
+        <?php echo  (int)($_POST['eth']), (int)($_POST['usb3']), (int)($_POST['pwr']); ?>
+        <?php if ((int)($_POST['eth']) == 1 and (int)($_POST['usb3']) == 1 and (int)($_POST['pwr']) == 1): ?>
             you have a raspberry pi 4 B+ model
         <?php endif; ?>
 
