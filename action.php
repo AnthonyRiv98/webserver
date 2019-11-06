05dbccbae24e3e928c7cd428b06ebd1daf7bdb2f
AnthonyRiv98 <55204439+AnthonyRiv98@users.noreply.github.com>
2019-11-06 14:43:19 -0500

    Update action.php

--- action.php
+++ action.php
@@ -8,7 +8,11 @@
         </div>
 
         <?php if ((int)($_POST['eth']) == 1 and (int)($_POST['usb3']) == 1 and (int)($_POST['pwr']) == 1): ?>
-            you have a raspberry pi 4 B+ model
+            you have a raspberry pi 4 model
+        <?php if ((int)($_POST['eth']) == 0 and (int)($_POST['usb3']) == 0 and (int)($_POST['pwr']) == 0): ?>
+            you have a raspberry pi 3 A+ model
+        <?php if ((int)($_POST['eth']) == 1 and (int)($_POST['usb3']) == 0 and (int)($_POST['pwr']) == 0): ?>
+            you have a raspberry pi 3 B+ model
         <?php endif; ?>
 
     </head>
